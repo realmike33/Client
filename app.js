6d7340934892ff12faea41b00aed3d51f76d9172
Mike <realmike33@gmail.com>
2015-02-12 09:49:18 -0800

    (Feat) Created basic index.html

---
+++ app.js
@@ -0,0 +1,26 @@
+var app = angular.module('Daas', [
+  'ui.router',
+  'Daas.home',
+  'Daas.login',
+  'Daas.signup'
+  ])
+
+.config(function($stateProvider, $urlRouterProvider){
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'home/home.html',
+      controller: 'HomeController'
+    })
+    .state('login', {
+      url:'/login',
+    	templateUrl: 'login/login.html',
+      controller: 'LoginController'
+    })
+    .state('signup', {
+      url:'/signup',
+      templateUrl: 'signup/signup.html',
+      controller: 'SignupController'
+    });
+  $urlRouterProvider.otherwise('/');
+});
