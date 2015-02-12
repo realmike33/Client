492cef9e6a6e553a0dbed7d497f837cb0a91ed32
Mike <realmike33@gmail.com>
2015-02-12 14:14:09 -0800

    (feat) Created landing page

--- signup/SignupController.js
+++ signup/SignupController.js
@@ -1,7 +1,14 @@
 angular.module('Daas.signup', [])
 
-.controller('SignupController', function($scope, $state){
-  $scope.next = function(){
-    $state.go('login');
-  };
+.controller('SignupController', function($scope, $state, Auth){
+  $scope.register = function(){
+  	if($scope.username && $scope.password){
+  	 var obj = {};
+     obj.username = $scope.username;
+     obj.password = $scope.password;
+     console.log(obj);
+    }else{
+      console.log('fucked');
+    }
+  }
 });
