2a53a34330bfda10eaf99a89d6acf5a0cab03244
Mike <realmike33@gmail.com>
2015-02-13 17:06:28 -0800

    (feat) Elements are now draggable!

--- app/dashboardCreator/dashboardCreator.js
+++ app/dashboardCreator/dashboardCreator.js
@@ -2,24 +2,19 @@
   angular.module('Daas.dashboardDirective', [])
 
   .directive('pTag', function(){
-    var template = '<p>hello</p>'
+    var template = '<div id="drag">hello</div>';
     return {
       restrict: 'E',
-      template: template
+      template: template,
+      controller: function(){
+         var draggables = Draggable.create('#drag', {
+            bounds: '#p-container',
+            edgeResistance: 0.65,
+            type: 'x,y'
+          });
+        var myDraggable = draggables[0];
+        myDraggable.enabled(true);
+      }
     }
   })
-  .directive('divTag', function(){
-    var template = '<div>Hi<span>Yo<span></div>'
-    return {
-      restrict: 'E',
-      template: template
-    }
-  })
-  .directive('buttonTag', function(){
-    var template = '<button>Hello</button>'
-    return {
-      restrict: 'E',
-      template: template
-    }
-  });
 })();
