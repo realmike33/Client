1f2c2397352eb9015013600f8edf68c032f16f7c
Mike <realmike33@gmail.com>
2015-02-16 15:55:48 -0800

    Travis Functionality

--- app/home/HomeController.js
+++ app/home/HomeController.js
@@ -1,7 +1,19 @@
 angular.module('Daas.home', [])
 
-.controller('HomeController', function($scope, $state, Auth){
+.controller('HomeController', function($scope, $state, Auth, $http){
   $scope.next = function(){
     $state.go('signup');
   };
+  var data = {username: 'yeah', password: 'ye'};
+  $scope.test = function(){
+    $http({
+      method: 'POST',
+      url: 'http://localhost:4500/login',
+      data: data
+    }).then(function(resp){
+      console.log(resp);
+    }).catch(function(err){
+      console.log('Your ', err);
+    })
+  }
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,55 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
-var reload = browserSync.reload;
 var watch = require('gulp-watch');
 var jshint = require('gulp-jshint');
 var shell = require('gulp-shell');
 var stylus = require('gulp-stylus');
+var ngAnnotate = require('gulp-ng-annotate');
+var minifyHTML = require('gulp-minify-html');
+var minifyCSS = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
 
 var paths = {
     scripts: ['./app/**/*.js', '!./app/lib/**/*.js'],
     styles: ['./styles/**/*.styl', './styles/main.styl'],
     test: ['./specs/**/*.js'],
-    html: ['./app/**/*.html']
+    html: ['./app/**/*.html'],
+    min: ['./min'],
+    uglify: ['./min/**/*.js']
 };
+
+gulp.task('minify-css', function(){
+  gulp.src('./app/styles/**/*.css')
+      .pipe(minifyCSS({keepBreaks:true}))
+      .pipe(gulp.dest(paths.min[0]));
+});
+
+
+gulp.task('minify-html', function(){
+  var opt = {
+    empty: true,
+    cdata: true,
+    spare: true,
+    quotes: true
+  };
+
+  gulp.src(paths.html)
+      .pipe(minifyHTML(opt))
+      .pipe(gulp.dest(paths.min[0]))
+});
+
+gulp.task('uglify', function(){
+  gulp.src(paths.uglify)
+  .pipe(uglify())
+  .pipe(gulp.dest(paths.min[0]));
+});
+
+
+gulp.task('minify', function(){
+  return gulp.src(paths.scripts)
+             .pipe(ngAnnotate())
+             .pipe(gulp.dest(paths.min[0]))
+});
 
 //compiles stylus into basic css
 gulp.task('stylus', function(){
@@ -20,25 +58,31 @@ gulp.task('stylus', function(){
               .pipe(gulp.dest('./app/styles'));
 });
 
+//lints js files
 gulp.task('jshint', function(){
     gulp.src(paths.scripts)
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'));
 })
 
+//serves static assets and auto-reloads browser when files change
+var reload = browserSync.reload;
 gulp.task('watch', ['serve'], function(){
   gulp.watch(paths.scripts, ['jshint', reload]).on('change', reportChange);
   gulp.watch(paths.styles[0], ['stylus', reload]).on('change', reportChange);
+  gulp.watch(paths.html, [reload]).on('change', reportChange);
 });
 
+//runs tests
 gulp.task('karma', shell.task(['karma start']));
 
+//shows in terminal when browser reloads
 function reportChange(event){
   console.log('File ' + event.path + 'was ' + event.type + ', running tasks...');
 };
 
 //serves static files, like a basic node server
-gulp.task('serve', ['build'], function(done){
+gulp.task('serve', function(done){
   browserSync({
     port: 3000,
     server: {
@@ -51,4 +95,5 @@ gulp.task('serve', ['build'], function(done){
   }, done);
 });
 
-gulp.task('build', ['jshint', 'stylus']);
+//build tool for deployment
+
